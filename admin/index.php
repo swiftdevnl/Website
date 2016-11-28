d4e0a2eab5598cbd2329acd424f55fc6f91984e9
Michiel Overtoom <motoom@xs4all.nl>
2016-11-28 17:27:04 +0100

    Admin

---
+++ admin/index.php
@@ -0,0 +1,10 @@
+<?php
+$metatitle = $metadescription = $metakeywords = $metaauthor = $contenttitle = "";
+$content = "<h1>Admin</h1>".
+"<p>Voor de volgende beheertaken zul je toegangsgegevens nodig hebben.".
+"<ul>".
+"<li><a href=\"agenda.php\">Agenda voor woensdag en zondag</a></li>".
+"<li><a href=\"bulkmail.php\">Mailing naar abonnees</a></li>".
+"<li><a href=\"#\">TODO: Feed met Swift-gerelateerde links</a></li>".
+"</ul>";
+include("../templates/artikel.php");
