d0ed4e0e1cf1ff87e6ab71d64f50f4aef3d52af4
Michiel Overtoom <motoom@xs4all.nl>
2016-11-28 17:28:12 +0100

    Forum melding

---
+++ forum.php
@@ -0,0 +1,4 @@
+<?php
+$metatitle = $metadescription = $metakeywords = $metaauthor = $contenttitle = "";
+$content = "<h1>Forum</h1><p>Het forum werd zo weinig gebruikt, dat we het uit hebben gezet.</p><p>Content die wel regelmatig bijgehouden werd, zoals de Swift-gerelateerde links van Digitist, en de agenda en videos van de zondag- en woensdag sessies, hebben een plekje op de <a href=\"/\">homepage</a> gekregen.</p><p>De interactiviteit van de SwiftDev-leden vindt tegenwoordig plaats op het Discord chatkanaal en de zondag/woensdag online sessies.</p><p>Vergeet je ook niet aan te melden voor de nieuwsbrief, als je nog niet geabonneerd bent. Hierin wordt ondermeer de agenda voor de online sessies aangekondigd.</p>";
+include("templates/artikel.php");
